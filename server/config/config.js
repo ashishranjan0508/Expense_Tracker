837d7642fd0f8d298db4bcf4fe51b87aee523227
Ashish <ashishranjan2052@gmail.com>
2025-08-16 14:57:37 +0530

    2nd Initial commit

---
+++ server/config/config.js
@@ -0,0 +1,25 @@
+require('dotenv').config();
+
+module.exports = {
+  development: {
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    host: process.env.DB_HOST,
+    dialect: 'postgres'
+  },
+  test: {
+    username: process.env.TEST_DB_USERNAME || 'root',
+    password: process.env.TEST_DB_PASSWORD || null,
+    database: process.env.TEST_DB_NAME || 'database_test',
+    host: process.env.TEST_DB_HOST || '127.0.0.1',
+    dialect: 'mysql'
+  },
+  production: {
+    username: process.env.PROD_DB_USERNAME || 'root',
+    password: process.env.PROD_DB_PASSWORD || null,
+    database: process.env.PROD_DB_NAME || 'database_production',
+    host: process.env.PROD_DB_HOST || '127.0.0.1',
+    dialect: 'mysql'
+  }
+};
