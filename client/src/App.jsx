ca685de82a8b4ecee11805ed8bb1dc492772b66c
Ashish <ashishranjan2052@gmail.com>
2025-09-27 22:01:06 +0530

    created config folder and config-route file where i have created a static config and updated App.jsx according to config-route.js

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,15 +1,8 @@
-import {lazy, Suspense} from "react";
+import { Suspense } from "react";
 import {BrowserRouter, Routes, Route} from "react-router-dom";
-
-import Home from './pages/Home.jsx'
-const Signup = lazy(() => { return (import('./pages/Signup.jsx')) });
-const Login = lazy(() => { return (import('./pages/login.jsx')) });
-const Dashboard = lazy( () => { return (import('./pages/Dashboard.jsx')) });
-const Budget = lazy( () => { return (import('./pages/Budget.jsx')) });
-const Expenses = lazy( () => { return (import('./pages/Expenses.jsx')) });
-const Income = lazy( () => { return (import('./pages/Income.jsx')) });
-
 import ProtectedRoute from "./context/ProtectedRoute.jsx";
+import { PUBLIC_ROUTES, PROTECTED_ROUTES } from "./config/route-config.js";
+import Home from "./pages/Home.jsx";
 
 
 function App() {
@@ -19,57 +12,45 @@ function App() {
     <BrowserRouter>
 
      <Routes>
-      <Route path="/" element={<Home />} />
-    
-      <Route path="/signup" element={ <Suspense fallback={
+     <Route path="/" element = {<Home/>} />
+
+     {PUBLIC_ROUTES.map( ({path, component : Component}) => {
+      return (
+        <Route key = {path}
+         path={path}
+         element={ <Suspense fallback={
          <div className="flex justify-center items-center h-screen">
          <div className="text-xl font-bold">Loading...</div></div> }> 
-         <Signup /></Suspense> }
+         <Component/>
+         </Suspense> }
        />
+      )
+     }
+     )}
 
-      <Route path="/login" element={ <Suspense fallback={
-         <div className="flex justify-center items-center h-screen">
-         <div className="text-xl font-bold">Loading...</div></div> }> 
-         <Login /></Suspense> }
-       />
 
-      <Route path="/dashboard" element={
-        <ProtectedRoute>
+     {PROTECTED_ROUTES.map(
+      ({path, component : Component}) => {
+        return(
+         <Route
+         key={path}
+         path={path}
+         element = {
+          <ProtectedRoute>
           <Suspense fallback = {
-         <div className="flex justify-center items-center h-screen">
-         <div className="text-xl font-bold">Loading...</div></div>
-          }><Dashboard /></Suspense>        
-        </ProtectedRoute>  } />
-
-      <Route path="/budget" element={
-        <ProtectedRoute>
-          <Suspense fallback = {
-         <div className="flex justify-center items-center h-screen">
-         <div className="text-xl font-bold">Loading...</div></div>
-          }><Budget /></Suspense>        
-        </ProtectedRoute>  } />
-
-      <Route path="/expenses" element={
-        <ProtectedRoute>
-          <Suspense fallback = {
-         <div className="flex justify-center items-center h-screen">
-         <div className="text-xl font-bold">Loading...</div></div>
-          }><Expenses/></Suspense>
-        </ProtectedRoute>
-      } />
-
-     <Route path="/income" element={
-         <ProtectedRoute>
-          <Suspense fallback = {
-         <div className="flex justify-center items-center h-screen">
-         <div className="text-xl font-bold">Loading...</div></div>
-         }> <Income /></Suspense>
-         </ProtectedRoute>
-       } />
+          <div className="flex justify-center items-center h-screen">
+          <div className="text-xl font-bold">Loading...</div></div>
+          }>
+           <Component/>
+          </Suspense>
+          </ProtectedRoute>
+         }
+         />
+        )
+      }
+     )}
        
      </Routes>
-
-
    </BrowserRouter>
 
   )
---
+++ client/src/config/route-config.js
@@ -0,0 +1,21 @@
+import { lazy } from "react"
+
+const Signup = lazy(() => { return (import('../pages/Signup.jsx')) });
+const Login = lazy(() => { return (import('../pages/login.jsx')) });
+const Dashboard = lazy( () => { return (import('../pages/Dashboard.jsx')) });
+const Budget = lazy( () => { return (import('../pages/Budget.jsx')) });
+const Expenses = lazy( () => { return (import('../pages/Expenses.jsx')) });
+const Income = lazy( () => { return (import('../pages/Income.jsx')) });
+
+export const PUBLIC_ROUTES = [
+    {path : '/signup', component: Signup},
+    {path : '/login', component: Login},
+]
+
+export const PROTECTED_ROUTES = [
+    {path : '/dashboard', component : Dashboard},
+    {path : '/budget', component : Budget},
+    {path : '/expenses', component : Expenses},
+    {path : '/income', component : Income}
+]
+
