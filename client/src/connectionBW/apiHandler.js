122cae9b7a29f5e98fb24bc5d7789bc6cbd04409
Ashish <ashishranjan2052@gmail.com>
2025-10-06 18:14:29 +0530

    apiHandler and .env file in client updated and added

--- client/src/connectionBW/apiHandler.js
+++ client/src/connectionBW/apiHandler.js
@@ -1,4 +1,6 @@
-const Baseurl = "http://localhost:5000/api";
+// const Baseurl = process.env.VITE_API_BASE_URL;
+const Baseurl = import.meta.env.VITE_API_BASE_URL;
+// "http://localhost:5000/api";
 
 export const apiCall = async (endpoint, config = {}) => {
    try {
@@ -10,3 +12,19 @@ export const apiCall = async (endpoint, config = {}) => {
        return { ok: false, data: { error: "Network error" } };
    }
 }
+
+// const Baseurl = process.env.REACT_APP_API_URL;
+// if (!Baseurl) {
+//   console.error("VITE_API_BASE_URL is not set. Please check your .env file.");
+// }
+// export const apiCall = async (endpoint, config = {}) => {
+//     try {
+
+//         const response = await fetch(`${Baseurl}${endpoint}`, config);
+//         const data = await response.json();
+//         return { ok: response.ok, data };
+//     } catch (error) {
+//         console.error("Api call failed", error);
+//         return { ok: false, data: { error: "Network error" } };
+//     }
+// }
