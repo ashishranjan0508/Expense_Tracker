58df53bee61785d2403902c3d125a3d7f8274f54
Ashish <ashishranjan2052@gmail.com>
2025-10-06 16:44:58 +0530

    Db problem using migration

--- server/server.js
+++ server/server.js
@@ -3,16 +3,6 @@ const dotenv = require('dotenv');
 const cors = require('cors');
 const path = require('path');
 
-// --- 1. IMPORT YOUR DATABASE CONNECTION ---
-const sequelize = require('./config/db.js'); 
-
-require('./models/userModel.js');
-require('./models/budgetsModel.js');
-require('./models/expensesModel.js');
-require('./models/incomeModel.js');
-
-
-
 const userRoutes = require('./routes/userRoutes.js');
 // ...other route imports...
 
@@ -20,7 +10,6 @@ dotenv.config();
 
 const app = express();
 
-// --- CORS CONFIGURATION ---
 const allowedOrigins = [
   process.env.FRONTEND_URL,
   'http://localhost:5173',
@@ -50,51 +39,7 @@ app.use('/api/users', userRoutes);
 
 const PORT = process.env.PORT || 5000;
 
-
-sequelize.sync()
-  .then(() => {
-    console.log("Database synced successfully.");
-    // We only start the server after the database is ready.
-    app.listen(PORT, () => {
-      console.log(`Server running on port ${PORT}`);
-    });
-  })
-  .catch(err => {
-    console.error('Unable to sync database:', err);
-  });
-
-
-
-
-
-
-
-
-
-
-
-// const express = require('express');
-// const dotenv = require('dotenv');
-// const cors = require('cors');
-// const path = require('path');
-
-// const userRoutes = require('./routes/userRoutes.js');
-// // ...other route imports...
-
-// dotenv.config();
-
-// const app = express();
-// app.use(cors());
-// app.use(express.json());
-
-// // Register API routes
-// app.use('/api/users', userRoutes);
-// // ...other routes...
-
-
-// const PORT = process.env.PORT || 5000;
-// app.listen(PORT, () => {
-//   console.log(`Server running on port ${PORT}`);
-// });
-
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
 
