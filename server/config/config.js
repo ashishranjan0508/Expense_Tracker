d9e1d8c5f3b0d05e7bb643680ec27eac1506d3e0
Ashish <ashishranjan2052@gmail.com>
2025-10-06 17:17:47 +0530

    config.js file change

--- server/config/config.js
+++ server/config/config.js
@@ -2,24 +2,47 @@ require('dotenv').config();
 
 module.exports = {
   development: {
-    username: process.env.DB_USERNAME,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
-    host: process.env.DB_HOST,
-    dialect: 'postgres'
+    use_env_variable: 'DB_URL',
+    dialect: 'postgres',
   },
   test: {
-    username: process.env.TEST_DB_USERNAME || 'root',
-    password: process.env.TEST_DB_PASSWORD || null,
-    database: process.env.TEST_DB_NAME || 'database_test',
-    host: process.env.TEST_DB_HOST || '127.0.0.1',
-    dialect: 'mysql'
+    use_env_variable: 'DB_URL',
+    dialect: 'postgres',
   },
   production: {
-    username: process.env.PROD_DB_USERNAME || 'root',
-    password: process.env.PROD_DB_PASSWORD || null,
-    database: process.env.PROD_DB_NAME || 'database_production',
-    host: process.env.PROD_DB_HOST || '127.0.0.1',
-    dialect: 'mysql'
-  }
+    use_env_variable: 'DB_URL',
+    dialect: 'postgres',
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false,
+      },
+    },
+  },
 };
+
+// require('dotenv').config();
+
+// module.exports = {
+//   development: {
+//     username: process.env.DB_USERNAME,
+//     password: process.env.DB_PASSWORD,
+//     database: process.env.DB_NAME,
+//     host: process.env.DB_HOST,
+//     dialect: 'postgres'
+//   },
+//   test: {
+//     username: process.env.TEST_DB_USERNAME || 'root',
+//     password: process.env.TEST_DB_PASSWORD || null,
+//     database: process.env.TEST_DB_NAME || 'database_test',
+//     host: process.env.TEST_DB_HOST || '127.0.0.1',
+//     dialect: 'mysql'
+//   },
+//   production: {
+//     username: process.env.PROD_DB_USERNAME || 'root',
+//     password: process.env.PROD_DB_PASSWORD || null,
+//     database: process.env.PROD_DB_NAME || 'database_production',
+//     host: process.env.PROD_DB_HOST || '127.0.0.1',
+//     dialect: 'mysql'
+//   }
+// };
